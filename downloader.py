98911578b6743c24080cd68a78f8298f860c7a98
Eddie Lee <eddieberklee@gmail.com>
2013-05-26 13:16:19 -0700

    so far seems to be working, just run python downloader.py

--- downloader.py
+++ downloader.py
@@ -1,12 +1,48 @@
 from bs4 import BeautifulSoup
-import urllib
+import urllib,urllib2
 import os
+
+ROOT = 'http://www.mangareader.net'
 
 def parseUrl(url):
     parts = url.split('/')
+    if parts[4] and not parts[5]:
+        return [parts[3], parts[4], '1']
     return [parts[3], parts[4], parts[5]]
 
+def findAndDownloadImage(opener, url):
+    manga, chapter, page = parseUrl(url)
+
+    f = opener.open(url)
+    html = f.read()
+    soup = BeautifulSoup(html)
+
+    image_src = soup.find('img',id='img')
+    if image_src:
+        image_src = image_src['src']
+        image_type = image_src.split('.')[-1]
+
+        directory = manga + '/' + chapter
+        if not os.path.exists(directory):
+                os.makedirs(directory)
+        print directory + '/' + page + '.' + image_type
+        image = open(directory + '/' + page + '.' + image_type, 'wb')
+        image.write(opener.open(image_src).read())
+        image.close()
+
+def findNextPage(opener, url):
+    html = opener.open(url).read()
+    soup = BeautifulSoup(html)
+    image = soup.find('img',id='img')
+    if image:
+        next_page = image.parent['href']
+        return next_page
+    else:
+        return False
+
 if __name__=='__main__':
+    opener = urllib2.build_opener()
+    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
 
     # query = 'shingeki'
 
@@ -18,27 +54,20 @@ if __name__=='__main__':
 
     url = "http://www.mangareader.net/" + manga + "/" + chapter + "/" + page
 
-    f = urllib.urlopen(url)
-    html = f.read()
-    soup = BeautifulSoup(html)
-
-    print soup
-
-    image_src = soup.find('',id='img')['src']
-
-    directory = manga
-    if not os.path.exists(directory):
-            os.makedirs(directory)
-    image = open('', 'wb')
-    urllib.urlopen(image_src)
+    findAndDownloadImage(opener, url)
 
     # find next page
-
-    # what does the last page of a series look like?
-
-    manga, chapter, page = parseUrl(url)
-
-    print manga, chapter, page
+    next_page = findNextPage(opener, url)
+    while next_page:
+        # set next_page to False if there is no next page aka when there is no <img id='img'
+        next_page = findNextPage(opener, url)
+        url = ROOT + next_page
+        manga, chapter, page = parseUrl(url)
+        findAndDownloadImage(opener, url)
+        # print manga, chapter, page, "was saved!"
+        pass
+    
+    print "Reached the end of the manga series at chapter %s page %s" % (chapter, page)
 
 
 
